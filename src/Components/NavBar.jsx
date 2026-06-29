8a25fa594c8465b525e4ebfaae22437123becbb2
Hawa-codes <adediranhawa@gmail.com>
2026-06-29 23:48:46 +0100

    Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,8 @@
-// import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 // import logo from '../assets/Frame.png'
-// import user from '../assets/user.svg'
-// import bag from '../assets/bag-2.svg'
+import user from '../assets/user.svg'
+import bag from '../assets/bag-2.svg'
+import logo from '../assets/Frame 1.svg'
 import './Navbar.css'
 
 const NavBar = () => {
@@ -9,9 +10,19 @@ const NavBar = () => {
     // const navigate = useNavigate()
 
   return (
-    <header className='logo'>
-
-    </header>
+    <nav className='navbar'>
+      <img src={logo} alt="logo" className='logo' />
+      <div className='desktopMenu'>
+        <Link className="desktopMenuListItem">Product</Link>
+        <Link className="desktopMenuListItem">Services</Link>
+        <Link className="desktopMenuListItem">Articles</Link>
+        <Link className="desktopMenuListItem">About Us</Link>
+      </div>
+      <div className='desktopMenuBtn'>
+        <img src={bag} alt="bag" className='desktopMenuImg' />
+        <img src={user} alt="user" className='desktopMenuImg' />
+      </div>
+    </nav>
   )
 }
 
